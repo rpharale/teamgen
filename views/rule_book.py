e036114b2158c27e70f57ecc43dfb5cc1005bbb2
rpharale <15881101+rpharale@users.noreply.github.com>
2022-11-02 16:59:32 -0700

    Updating rule_book.py

--- views/rule_book.py
+++ views/rule_book.py
@@ -1,9 +1,17 @@
 import streamlit as st
+from pathlib import Path
 
+RULE_BOOK_PATH = "./data/rules.md"
+
+def read_markdown_file(file_path):
+    return Path(file_path).read_text()
 
 class RuleBook:
     class Model:
-        pageTitle = "Rule Book"
+        pageTitle = ""
 
     def view(self, model):
         st.title(model.pageTitle)
+
+        markdown_text = read_markdown_file(RULE_BOOK_PATH)
+        st.markdown(markdown_text, unsafe_allow_html=True)
